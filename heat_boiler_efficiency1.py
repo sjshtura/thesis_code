2da63a3f1307b975d4fcbe89bd2fea47656d8d3e
Shakhawat Hossain Turag <shakhawat.turag@stud.uni-due.de>
2021-09-02 06:03:55 +0200

    new update

---
+++ heat_boiler_efficiency1.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+import datetime
+
+df_data = pd.read_excel("when2heat_DE.xlsx",sheet_name='Sheet2',usecols = "A,B,C,J",skiprows=3)
+
+df_data["Unnamed: 1"]= df_data["Unnamed: 1"].str.slice(start = 11)
+df_data["unit"]= df_data["unit"].str.slice(stop = 10)
+sum_column = df_data["MW"] + df_data["MW.7"]
+df_data["heat_demand"] = sum_column
+
+
+df_data = df_data.drop(['MW','MW.7'], axis = 1)
+df_data.unit =  pd.to_datetime(df_data.unit, errors='ignore')
+df_data
+
+df_data.columns = ["date", "hour", "heat_demand"]
+# df_data['date'] = df_data['date'].dt.date
+df_data.hour =  pd.to_datetime(df_data.hour, errors='ignore', format="%H:%M:%S").dt.time
+df_data
+
+df_data = df_data.drop_duplicates(['date', 'hour'])
+df_data
+heat_demand_pivot = df_data.pivot(index = 'date', columns = 'hour', values = 'heat_demand')
+heat_demand_pivot
+heat_eff = heat_demand_pivot
+
+heat_eff.loc[:] = 0.90
+print("heat efficiency\n")
+print(heat_eff.head())
